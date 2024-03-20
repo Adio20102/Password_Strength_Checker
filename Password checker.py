122d8b67e537acc2ed381a5d06183354656378a8
Aditya Joshi <117490641+Adio20102@users.noreply.github.com>
2024-03-20 19:36:49 +0530

    Update Password checker.py

--- Password checker.py
+++ Password checker.py
@@ -5,41 +5,45 @@ def checkPassword(password):
     upperChars, lowerChars, specialChars, digits, length, spaces = 0, 0, 0, 0, 0, 0
     password = password.strip()
     length = len(password)
-
+    messages = []
     if(length==0):
-        messagebox.showwarning("Password Strength Checker", "Password must not be blank!")
-        return
-    elif (length < 8):
-        messagebox.showwarning("Password Strength Checker", "Password must be at least 8 characters long!")
-        return
-    else:
-        for i in range(0, length):
-            if (password[i].isupper()):
-                upperChars += 1
-            elif (password[i].islower()):
-                lowerChars += 1
-            elif (password[i].isdigit()):
-                digits += 1
-            else:
-                specialChars += 1
+        messages.append("Password must not be blank!")
 
-    if (upperChars != 0 and lowerChars != 0 and digits != 0 and specialChars != 0):
-        if (length >= 10):
-            messagebox.showinfo("Password Strength Checker", "Password is strong.")
+    elif (length < 8):
+        messages.append("Password must be at least 8 characters long!")
+    blank_space = 0
+    for i in range(0, length):
+        if password[i] == " ":
+            messages.append("Password must not contain blank spaces!")
+            blank_space = 1
+            break
+    for i in range(0, length):
+        if password[i].isupper():
+            upperChars += 1
+        elif password[i].islower():
+            lowerChars += 1
+        elif password[i].isdigit():
+            digits += 1
         else:
+            specialChars += 1
+
+    if upperChars != 0 and lowerChars != 0 and digits != 0 and specialChars != 0 and blank_space == 0:
+        if length >= 10:
+            messagebox.showinfo("Password Strength Checker", "Password is strong.")
+        elif 8 <= length < 10:
             messagebox.showinfo("Password Strength Checker", "Password is of medium strength.")
     else:
-        messages = []
-        if (upperChars == 0):
-            messages.append("Password must contain at least one uppercase character!")
-        if (lowerChars == 0):
-            messages.append("Password must contain at least one lowercase character!")
-        if (specialChars == 0):
-            messages.append("Password must contain at least one special character!")
-        if (digits == 0):
-            messages.append("Password must contain at least one digit!")
 
-        messagebox.showwarning("Password Strength Checker", "\n".join(messages))
+        if upperChars == 0:
+            messages.append("Password must contain at least one uppercase character!")
+        if lowerChars == 0:
+            messages.append("Password must contain at least one lowercase character!")
+        if specialChars == 0:
+            messages.append("Password must contain at least one special character!")
+        if digits == 0:
+            messages.append("Password must contain at least one digit!")
+    if len(messages) > 0:
+     messagebox.showwarning("Password Strength Checker", "\n".join(messages))
 
 def on_check_password():
     password = password_entry.get()
